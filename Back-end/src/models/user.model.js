026d6a50f568d3877cb6694075214eca3352556e
Edgar Andrés <76711142+MassterProg@users.noreply.github.com>
2021-06-05 21:47:11 -0500

    Agregue algunos comentarios

--- Back-end/src/models/user.model.js
+++ Back-end/src/models/user.model.js
@@ -1,4 +1,4 @@
-
+ 
 module.exports = mongoose => {
     var schema = mongoose.Schema(
       {
--- Back-end/src/routes/users.routes.js
+++ Back-end/src/routes/users.routes.js
@@ -1,25 +1,26 @@
-module.exports = app => {
-    const users = require("../controllers/user.controller");
-  
-    var router = require("express").Router();
-  
-    // Create a new Tutorial
-    router.post("/", users.create);
+module.exports = (app) => {
+  const users = require("../controllers/user.controller");
 
-    router.post("/auth", users.login);
-  
-    // Retrieve all users
-    router.get("/", users.findAll);
-  
-    // Retrieve a single Tutorial with id
-    router.get("/:id", users.findOne);
-  
-    // Update a Tutorial with id
-    router.put("/:id", users.update);
-  
-    // Delete a Tutorial with id
-    router.delete("/:id", users.delete);
+  var router = require("express").Router();
 
-    app.use('/users', router);
-  
-  };
+  // Create a new Tutorial
+  router.post("/", users.create);
+
+  router.post("/auth", users.login);
+
+  // Retrieve all users
+  router.get("/", users.findAll);
+
+  // Retrieve a single Tutorial with id
+  router.get("/:id", users.findOne);
+
+  // Update a Tutorial with id
+  router.put("/:id", users.update);
+
+  // Delete a Tutorial with id
+  router.delete("/:id", users.delete);
+
+  app.use("/users", router);
+
+  //me parece que aqui iria la ruta user/me para que de aqui mandarlo a hacer toda esa chamba del localstorege
+};
