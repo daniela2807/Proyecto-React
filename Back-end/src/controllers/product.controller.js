bca1a0f9890fd7b7b12ac70a972e4f1247a117f4
Edgar Andrés <76711142+MassterProg@users.noreply.github.com>
2021-06-15 05:07:31 -0500

    backend de slider pero regresa puro null

--- Back-end/src/controllers/product.controller.js
+++ Back-end/src/controllers/product.controller.js
@@ -3,7 +3,7 @@ const Product = db.product;
 
 // Create and Save a new User
 exports.create = (req, res) => {
-     // Validate request
+  // Validate request
   if (!req.body.name || !req.body.brand || !req.body.cost || !req.body.id_cat) {
     res.status(400).send({ message: "Content can not be empty!" });
     return;
@@ -16,101 +16,99 @@ exports.create = (req, res) => {
     cost: req.body.cost,
     id_cat: req.body.id_cat,
     description: req.body.description,
-    img: req.body.img
+    img: req.body.img,
   });
 
   // Save Product in the database
   product
     .save(product)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
         message:
-          err.message || "Some error occurred while creating the Product."
+          err.message || "Some error occurred while creating the Product.",
       });
     });
-  
 };
 
 // Retrieve all Users from the database.
 exports.findAll = (req, res) => {
-    Product.find()
-    .then(data => {
-        if(!data){
-            res.send("No existen Productos")
-        }
+  Product.find()
+    .then((data) => {
+      if (!data) {
+        res.send("No existen Productos");
+      }
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving Users."
+        message: err.message || "Some error occurred while retrieving Users.",
       });
     });
 };
 
 // Find a single User with an id
 exports.findOne = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Product.findById(id)
-      .then(data => {
-        if (!data)
-          res.status(404).send({ message: "Not found Product with id " + id });
-        else res.send(data);
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .send({ message: "Error retrieving Product with id=" + id });
-      });
+  Product.findById(id)
+    .then((data) => {
+      if (!data)
+        res.status(404).send({ message: "Not found Product with id " + id });
+      else res.send(data);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Product with id=" + id });
+    });
 };
 
 // Update a User by the id in the request
 exports.update = (req, res) => {
-    if (!req.body) {
-        return res.status(400).send({
-          message: "Data to update can not be empty!"
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!",
+    });
+  }
+
+  const id = req.params.id;
+
+  Product.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Product with id=${id}. Maybe Product was not found!`,
         });
-      }
-    
-      const id = req.params.id;
-    
-      Product.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-        .then(data => {
-          if (!data) {
-            res.status(404).send({
-              message: `Cannot update Product with id=${id}. Maybe Product was not found!`
-            });
-          } else res.send({ message: "Product was updated successfully." });
-        })
-        .catch(err => {
-          res.status(500).send({
-            message: "Error updating Product with id=" + id
-          });
-        });
+      } else res.send({ message: "Product was updated successfully." });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating Product with id=" + id,
+      });
+    });
 };
 
 // Delete a User with the specified id in the request
 exports.delete = (req, res) => {
-    const id = req.params.id;
-    Product.findByIdAndRemove(id)
-      .then(data => {
-        if (!data) {
-          res.status(404).send({
-            message: `Cannot delete Product with id=${id}. Maybe Product was not found!`
-          });
-        } else {
-          res.send({
-            message: "Product was deleted successfully!"
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).send({
-          message: "Could not delete Product with id=" + id
+  const id = req.params.id;
+  Product.findByIdAndRemove(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Product with id=${id}. Maybe Product was not found!`,
         });
+      } else {
+        res.send({
+          message: "Product was deleted successfully!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete Product with id=" + id,
       });
+    });
 };
--- Back-end/src/models/index.js
+++ Back-end/src/models/index.js
@@ -11,6 +11,6 @@ db.product = require("./product.model")(mongoose);
 db.category = require("./category.model")(mongoose);
 db.address = require("./address.model")(mongoose);
 db.favorit = require("./favorites.model")(mongoose);
-
+db.slider = require("./slider.model")(mongoose);
 
 module.exports = db;
